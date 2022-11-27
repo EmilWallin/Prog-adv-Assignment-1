fa88d15dd4830fa5b060b96f88fe0033694815f0
Emil Wallin <43149212+EmilWallin@users.noreply.github.com>
2022-11-27 16:20:43 +0100

    Wallet and money added

---
+++ User/Values/OneMoney.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmilWallin_Inlämning_1.User.Values
+{
+    internal class OneMoney : Money
+    {
+        public OneMoney()
+        {
+            this.Value = 1;
+        }
+    }
+}
